3b180159b24e3c023fbdc2164bc455521c6481a1
igalna <igalna@gmail.com>
2015-07-07 18:53:42 +0100

    removed testing

--- ClaimsReserving/src/claimsreversing/OutputDataToCSVFile.java
+++ ClaimsReserving/src/claimsreversing/OutputDataToCSVFile.java
@@ -20,24 +20,6 @@ public class OutputDataToCSVFile {
 	private Map<String, Map<Integer, List<Product>>> inputData;
 	private CSVWriter writer;
 
-	public static <V> void main(String[] args) {
-		DataRowFormatter testFormatter = DataRowFormatter.of(new AccumulateData("input").getUnstructuredProductData());
-		TriangleCreator creator = TriangleCreator.of(testFormatter.formatInputDataIntoStructuredProducts());
-		OutputDataToCSVFile opd = new OutputDataToCSVFile("output", creator.createTrianglesFromInputData());
-		
-		Iterator it = opd.inputData.values().iterator();
-		
-		while (it.hasNext()) {
-			System.out.println(it.next().getClass());
-			@SuppressWarnings("unchecked")
-			Iterator<Product> it2 = (HashMap<> it.next())).iterator();
-			while (it2.hasNext()) {
-				System.out.println(it2.next().getClass());
-			}
-		}
-		
-		
-	}
 	
 	public OutputDataToCSVFile(String fileName, Map<String, Map<Integer, List<Product>>> inputData) {
 		this.inputData = inputData;
