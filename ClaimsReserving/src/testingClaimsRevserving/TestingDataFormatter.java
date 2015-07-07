9fe23dba8f0fe48ebe4fa2f622c03c449a2bf9af
igalna <igalna@gmail.com>
2015-07-07 11:43:01 +0100

    Testing the DataRowFormatter
    which converts the unstructured string data into Product objects with
    structured data types

---
+++ ClaimsReserving/src/testingClaimsRevserving/TestingDataFormatter.java
@@ -0,0 +1,38 @@
+package testingClaimsRevserving;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import claimsreversing.DataRowFormatter;
+import claimsreversing.AccumulateData;
+import claimsreversing.Product;
+
+public class TestingDataFormatter {
+
+	DataRowFormatter testFormatter;
+	List<Product> formattedData;
+	Product testProduct;
+	
+	@Before
+	public void buildBefore() {
+		testFormatter = DataRowFormatter.of(new AccumulateData("input").getUnstructuredProductData());
+		formattedData = testFormatter.dataFormatter();
+		testProduct = formattedData.get(0);
+		
+	}
+	
+	@Test
+	public void testProductIsString() {
+		assertEquals("Comp", testProduct.getProduct());
+	}
+	
+	@Test
+	public void testOriginYearIsInteger() {
+		assertEquals(Integer.valueOf(1992), testProduct.getOriginYear());
+	}
+
+}
