095e9cc034606f4e582cc0b7b3c81ad0c1b17e58
igalna <igalna@gmail.com>
2015-07-07 14:38:35 +0100

    Tests for Triangle creator
    Triangles are a Map data structure, they map the Product name to the
    origin year and a list of all the products within the triangle

---
+++ ClaimsReserving/src/testingClaimsRevserving/TestingTriangleCreator.java
@@ -0,0 +1,38 @@
+package testingClaimsRevserving;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import claimsreversing.AccumulateData;
+import claimsreversing.DataRowFormatter;
+import claimsreversing.Product;
+import claimsreversing.TriangleCreator;
+
+public class TestingTriangleCreator {
+
+	TriangleCreator creator;
+	DataRowFormatter testFormatter;
+	Map<String, Map<Integer, List<Product>>> testMap;
+	
+	@Before
+	public void buildBefore() {
+		testFormatter = DataRowFormatter.of(new AccumulateData("input").getUnstructuredProductData());
+		creator = TriangleCreator.of(testFormatter.formatInputDataIntoStructuredProducts());
+		testMap = creator.createTrianglesFromInputData();
+	}
+	
+	@Test
+	public void testGetNonCompKey() {
+		assertTrue(testMap.containsKey("Non-Comp"));
+	}
+	@Test
+	public void testGetCompKey() {
+		assertTrue(testMap.containsKey("Comp"));
+	}
+
+}
