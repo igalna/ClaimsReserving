e9e8a1056df775aed9bfae8f62f41057f3982b87
igalna <igalna@gmail.com>
2015-07-07 18:55:36 +0100

    JUnit testing for the new triangle creator

--- ClaimsReserving/src/testingClaimsRevserving/TestingTriangleCreator.java
+++ ClaimsReserving/src/testingClaimsRevserving/TestingTriangleCreator.java
@@ -1,43 +1,32 @@
 package testingClaimsRevserving;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import claimsreversing.AccumulateData;
 import claimsreversing.DataRowFormatter;
-import claimsreversing.Product;
+import claimsreversing.Triangle;
 import claimsreversing.TriangleCreator;
 
 public class TestingTriangleCreator {
 
 	TriangleCreator creator;
 	DataRowFormatter testFormatter;
-	Map<String, Map<Integer, List<Product>>> testMap;
+	List<Triangle> testList;
 	
 	@Before
 	public void buildBefore() {
 		testFormatter = DataRowFormatter.of(new AccumulateData("input").getUnstructuredProductData());
 		creator = TriangleCreator.of(testFormatter.formatInputDataIntoStructuredProducts());
-		testMap = creator.createTrianglesFromInputData();
-	}
-	
-	@Test
-	public void testGetNonCompKey() {
-		assertTrue(testMap.containsKey("Non-Comp"));
-	}
-	@Test
-	public void testGetCompKey() {
-		assertTrue(testMap.containsKey("Comp"));
+		testList = creator.createTrianglesFromInputData();
 	}
 
-	
 	@Test
-	public void testContainsYear() {
-		assertEquals(testMap.get("Non-Comp").get(1990).get(0).getOriginYear(), Integer.valueOf(1990));
+	public void testAddTriangle() {
+		assertEquals(testList.get(0).getProduct(), "Comp");
 	}
 }
