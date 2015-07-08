43b4bfeadaac0c0c603adabfa89a70316c7135c2
igalna <igalna@gmail.com>
2015-07-08 14:34:03 +0100

    TriangleCreator

--- ClaimsReserving/src/claimsreversing/TriangleCreator.java
+++ ClaimsReserving/src/claimsreversing/TriangleCreator.java
@@ -12,13 +12,9 @@ public class TriangleCreator {
 	
 	
 	public List<TriangleOfPaymentFigures> createTrianglesFromInputData() {
-		//Map<String, Map<Integer, List<Product>>> result;
-		//result = inputData.stream().collect(Collectors.groupingBy(Product::getProduct, Collectors.groupingBy(Product::getOriginYear)));
-		//return result;
 		
 		List<TriangleOfPaymentFigures> triangleList = new ArrayList<TriangleOfPaymentFigures>();
 	
-		
 		for (Product product : inputData) {
 			if (triangleList.isEmpty()) {
 				TriangleOfPaymentFigures newTriangle = TriangleOfPaymentFigures.of(product.getProduct(), product.getOriginYear(), new ArrayList<Product>());
